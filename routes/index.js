da6120fe5921bced6ec2b64be9113be5568cf241
Carl J. Bialorucki <cbialo2@outlook.com>
2023-01-06 17:35:53 -0700

    Cleaned up project

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,11 @@ const routes = require('express').Router();
 routes.get('/', (req, res) => {
   res.send('Aubrey Bialorucki');
 });
+routes.get('/test', (req, res) => {
+  res.send('Bonnie Bialorucki');
+});
+routes.get('/test2', (req, res) => {
+  res.send('Carl Bialorucki');
+});
 
 module.exports = routes;
